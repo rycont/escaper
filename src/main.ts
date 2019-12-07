818ad9e8dc39ab7fa6c3aeffceba922ea9626d7d
wjdgks1224 <wjdgks1224@gmail.com>
2019-12-07 21:17:33 +0900

    add set polyfill

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
+import Vue from 'vue'
+import App from './App.vue'
+import 'core-js/es/set'
 
-Vue.config.productionTip = false;
+import './registerServiceWorker'
+
+Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
